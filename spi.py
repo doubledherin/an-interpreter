34ed3aa0994ba0323fdf6671a50ce1df974a5616
Wendy Dherin <wendydherin@gmail.com>
2017-09-09 14:04:48 -0700

    Indent interpreter so it's clear it inherits from NodeVisitor.

--- spi.py
+++ spi.py
@@ -351,54 +351,54 @@ class NodeVisitor(object):
     def generic_visit(self, node):
         raise Exception('No visit_{} method'.format(type(node).__name__))
 
-class Interpreter(NodeVisitor):
+    class Interpreter(NodeVisitor):
 
-    GLOBAL_SCOPE = {}
-    
-    def __init__(self, parser):
-        self.parser = parser
+        GLOBAL_SCOPE = {}
 
-    def visit_UnaryOp(self, node):
-        if node.op.type == PLUS:
-            return +self.visit(node.expr)
-        elif node.op.type == MINUS:
-            return -self.visit(node.expr)
+        def __init__(self, parser):
+            self.parser = parser
 
-    def visit_BinOp(self, node):
-        if node.op.type == PLUS:
-            return self.visit(node.left) + self.visit(node.right)
-        elif node.op.type == MINUS:
-            return self.visit(node.left) - self.visit(node.right)
-        elif node.op.type == MULTIPLY:
-            return self.visit(node.left) * self.visit(node.right)
-        elif node.op.type == DIVIDE:
-            return self.visit(node.left) / self.visit(node.right)
+        def visit_UnaryOp(self, node):
+            if node.op.type == PLUS:
+                return +self.visit(node.expr)
+            elif node.op.type == MINUS:
+                return -self.visit(node.expr)
 
-    def visit_Num(self, node):
-        return node.value
+        def visit_BinOp(self, node):
+            if node.op.type == PLUS:
+                return self.visit(node.left) + self.visit(node.right)
+            elif node.op.type == MINUS:
+                return self.visit(node.left) - self.visit(node.right)
+            elif node.op.type == MULTIPLY:
+                return self.visit(node.left) * self.visit(node.right)
+            elif node.op.type == DIVIDE:
+                return self.visit(node.left) / self.visit(node.right)
 
-    def visit_Compound(self, node):
-        for child in node.children:
-            self.visit(child)
+        def visit_Num(self, node):
+            return node.value
 
-    def visit_NoOp(self, node):
-        pass
+        def visit_Compound(self, node):
+            for child in node.children:
+                self.visit(child)
 
-    def visit_Assign(self, node):
-        var_name = node.left.value
-        self.GLOBAL_SCOPE[var_name] = self.visit(node.right)
+        def visit_NoOp(self, node):
+            pass
 
-    def visit_Var(self, node):
-        var_name = node.value
-        val = self.GLOBAL_SCOPE.get(var_name)
-        if val is None:
-            raise NameError(repr(var_name))
-        else:
-            return val
+        def visit_Assign(self, node):
+            var_name = node.left.value
+            self.GLOBAL_SCOPE[var_name] = self.visit(node.right)
 
-    def interpret(self):
-        tree = self.parser.parse()
-        return self.visit(tree)
+        def visit_Var(self, node):
+            var_name = node.value
+            val = self.GLOBAL_SCOPE.get(var_name)
+            if val is None:
+                raise NameError(repr(var_name))
+            else:
+                return val
+
+        def interpret(self):
+            tree = self.parser.parse()
+            return self.visit(tree)
 
 
 def main():
